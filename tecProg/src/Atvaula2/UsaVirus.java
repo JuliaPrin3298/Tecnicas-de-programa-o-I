8397135139d9760caac2560be0963b5482f12308
JuliaPrin3298 <juliaprinnena1123@gmail.com>
2025-09-01 19:42:52 -0300

    Atz atv

--- tecProg/src/Atvaula2/UsaVirus.java
+++ tecProg/src/Atvaula2/UsaVirus.java
@@ -2,9 +2,11 @@ package Atvaula2;
 public class UsaVirus {
 
 	public static void main(String[] args) {
-		Virus objVirus = new Virus(001 , "AXC-IIL", "Complexos");
+		Virus objVirus = new Virus(001 , "AXC-IIL", "Complexos", 10.76);
 	
-
+			objVirus.mostrarNome();
+			objVirus.mostrarTipo();
+			objVirus.infoVirus();
 	}
 
 }
--- tecProg/src/Atvaula2/Virus.java
+++ tecProg/src/Atvaula2/Virus.java
@@ -4,20 +4,28 @@ public class Virus {
  private int id;
  private String nome;
  private String tipo;
+ private double taxaMorte;
  
- public Virus( int id, String nome, String tipo){
+ public Virus( int id, String nome, String tipo, double taxaMorte){
 		this.id =  id;
 		this.nome = nome;
 		this.tipo = tipo;
+		this.taxaMorte = taxaMorte;
 	}
  
- public void nome() {
+ public void mostrarNome() {
 	 System.out.println("O nome do vírus é "+ nome);
  }
  
- public void tipo() {
+ public void mostrarTipo() {
 	 System.out.println("O tipo do vírus é "+ tipo);
  }
  
- public void 
+ public void infoVirus(){
+	System.out.println("Vírus: "+ nome);
+	System.out.println("Tipo: "+ tipo);
+	System.out.println("Taxa de Mortalidade: "+ taxaMorte);
+	System.out.println("=================================");
+
+ }
 }
