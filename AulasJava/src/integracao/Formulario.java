8db4f4a051ca5b05dc9bc8d0e26777fffeccebb8
JuliaPrin3298 <juliaprinnena1123@gmail.com>
2025-10-13 11:07:25 -0300

    Aula 5

---
+++ AulasJava/src/integracao/Formulario.java
@@ -0,0 +1,35 @@
+package exemplo1_Janela;
+
+import javax.swing.*;
+
+public class Formulario extends JFrame {
+	
+	private static final long serialversionUID = 1L;
+	public Formulario() {
+		super();
+		
+		JTextField texto = new JTextField();
+		JButton botao = new JButton("OKAY");
+		JLabel mensagem = new JLabel("Inserindo Componentes");
+		
+		setLayout(null);
+		
+		add(texto);
+		add(botao);
+		add(mensagem);
+		
+		texto.setBounds(140, 20, 300, 20);
+		botao.setBounds(140, 50, 150, 20);
+		mensagem.setBounds(140, 80, 220, 20);
+		setSize(800, 370);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+	
+	public static void main(String[] args) {
+		new Formulario();
+		
+		
+	}
+}
---
+++ AulasJava/src/integracao/Mensagem.java
@@ -0,0 +1,29 @@
+package exemplo1_Janela;
+
+import java.awt.BorderLayout;
+import java.awt.*;
+
+import javax.swing.*;
+
+public class Mensagem {
+
+	public static void main(String[] args) {
+		JFrame janela = new JFrame("Mensagem");
+		JLabel texto  = new JLabel("Interface gráfica em Java");
+		janela.getContentPane().setBackground(new Color (213, 244, 232));
+		//Adicionando o rótulo (LABEL) a frame (janela)
+		janela.getContentPane().add(texto);
+		
+		janela.setLayout(null);//Define o layout nulo
+		
+		texto.setBounds(70,50,200,20);//Define o tamanho do rótulo
+		janela.add(texto, BorderLayout.CENTER);
+		//janela.getContentPane().setBackground(Color.RED); //cor de fundo
+		texto.setForeground(Color.BLUE);
+		janela.setSize(300, 150);
+		janela.setVisible(true);
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+
+}
